56547ba4ca606bb0b0249345efc6defb9bab0ef7
grklp3 <32232873+grklp3@users.noreply.github.com>
2018-08-29 03:44:10 -0400

    pclDyno check moved to Router component

--- src/App.js
+++ src/App.js
@@ -4,58 +4,13 @@ import Router from 'Router';
 import Footer from 'components/footer';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      loader: false
-    };
-  }
-  componentDidMount() {
-    if (!window.sessionStorage.pclDyno) {
-      this.setState({loader: true});
-      fetch('https://pin-cl-s-275.herokuapp.com/confirm-live', {
-        method: "POST",
-        mode: "cors",
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      }).then((res) => res.json()).then((json) => {
-        setTimeout(() => {
-          window.sessionStorage.setItem('pclDyno', 'loaded');
-          this.setState({loader: false});
-        }, 2000);
-      }).catch((e) => {
-        alert('Heroku server currently unavailable');
-        if (window.sessionStorage.pclDyno) {
-          window.sessionStorage.removeItem('pclDyno');
-        }
-      });
-    }
-  }
   render() {
-    if (this.state.loader) {
-      return (
-        <div className="App">
-          <div style={{textAlign: "center"}}>
-            <img
-              src={require('./img/loader.gif')}
-              alt=""
-              height="36px"
-              width="36px"
-              style={{marginTop: "30px"}}
-            />
-          </div>
-        </div>
-      );
-    } else {
-      return (
-        <div className="App">
-          <Router/>
-          <Footer/>
-        </div>
-      );
-
-  }
+    return (
+      <div className="App">
+        <Router/>
+        <Footer/>
+      </div>
+    );
 }
 
 export default App;
--- src/Router.js
+++ src/Router.js
@@ -13,22 +13,64 @@ import CreatePin from 'routes/create-pin';
 import CreateBoard from 'routes/create-board';
 
 class Router extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loader: false
+    };
+  }
+  componentDidMount() {
+    if (!window.sessionStorage.pclDyno) {
+      this.setState({loader: true});
+      fetch('https://pin-cl-s-275.herokuapp.com/confirm-live', {
+        method: "POST",
+        mode: "cors",
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }).then((res) => res.json()).then((json) => {
+        setTimeout(() => {
+          window.sessionStorage.setItem('pclDyno', 'loaded');
+          this.setState({loader: false});
+        }, 2000);
+      }).catch((e) => {
+        alert('Heroku server currently unavailable');
+        if (window.sessionStorage.pclDyno) {
+          window.sessionStorage.removeItem('pclDyno');
+        }
+      });
+    }
+  }
   render() {
     return (
-      <Switch>
-        <Route exact path="/" component={Main}/>
-        <Route path="/home" component={Home}/>
-        <Route path="/following" component={Following}/>
-        <Route path="/profile" component={Profile}/>
-        <Route path="/sign-up" component={SignUp}/>
-        <Route path="/log-in" component={LogIn}/>
-        <Route path="/board/new" component={CreateBoard}/>
-        <Route path="/pin/new" component={CreatePin}/>
-        <Route path="/board/view" component={Board}/>
-        <Route path="/pin/save/choose-board" component={ChooseBoard}/>
-        <Route path="/pin" component={Pin}/>
-        <Route path="*" render={() => <Redirect to="/home"/>}/>
-      </Switch>
+      <div>
+      {this.state.loader ?
+        <div style={{textAlign: "center"}}>
+          <img
+            src={require('./img/loader.gif')}
+            alt=""
+            height="36px"
+            width="36px"
+            style={{marginTop: "30px"}}
+          />
+        </div>
+        :
+        <Switch>
+          <Route exact path="/" component={Main}/>
+          <Route path="/home" component={Home}/>
+          <Route path="/following" component={Following}/>
+          <Route path="/profile" component={Profile}/>
+          <Route path="/sign-up" component={SignUp}/>
+          <Route path="/log-in" component={LogIn}/>
+          <Route path="/board/new" component={CreateBoard}/>
+          <Route path="/pin/new" component={CreatePin}/>
+          <Route path="/board/view" component={Board}/>
+          <Route path="/pin/save/choose-board" component={ChooseBoard}/>
+          <Route path="/pin" component={Pin}/>
+          <Route path="*" render={() => <Redirect to="/home"/>}/>
+        </Switch>
+      }
+      </div>
     );
   }
 }
